a8332cb030d23827ad0533d487dfe8fc0f1aeca4
Murad Alseidi <murad.saidi65@gmail.com>
2020-11-27 01:29:36 -0800

    feat:navigate recipes and shopping list with event binding and ngif

--- src/app/header/herder.component.ts
+++ src/app/header/herder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
   collapsed = true;
   constructor() {}
+  // tslint:disable-next-line: typedef
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
