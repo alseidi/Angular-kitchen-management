21e790a26ce295d4ef36e31785f3d1fc0495827f
Murad Alseidi <murad.saidi65@gmail.com>
2020-12-09 01:02:14 -0800

    refactor(module): auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
+import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { CoreModule } from './core.module';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
+    AuthModule,
+    AppRoutingModule,
   ],
   bootstrap: [AppComponent],
 })
